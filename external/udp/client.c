9f4e5b011c6878fbb29e857dd8c809875777878b
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-08-06 22:44:57 +0530

    done udp

--- external/udp/client.c
+++ external/udp/client.c
@@ -1,33 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<unistd.h>
 
-int main() {
+int main(){
     int client;
-    char buffer[1024];
     struct sockaddr_in servAddr;
-
-    client = socket(AF_INET, SOCK_DGRAM, 0);
-
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(6565);
-    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    while (1) {
-        printf("Enter the content to be sent to the server (type 'exit' to quit): ");
-        scanf("%s", buffer);
-        if (strcmp(buffer, "exit") == 0) {
+    // socklen_t clientAddrSize=sizeof(clientAddr);
+    char buffer[1024];
+    client=socket(AF_INET,SOCK_DGRAM,0);
+    servAddr.sin_family=AF_INET;
+    servAddr.sin_port=htons(6565);
+    servAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    if(client==-1){
+        printf("error\n");
+        exit(1);
+    }
+    else{
+        printf("socket created suceessfully..\n");
+    }
+    while(1){
+        printf("Enter the content to be send to server: \n");
+        scanf("%s",buffer);
+        if(strcmp(buffer,"exit")==0){
             break;
         }
-        sendto(client, buffer, strlen(buffer), 0, (struct sockaddr*)&servAddr, sizeof(servAddr));
-        recvfrom(client, buffer, 1024, 0, NULL, NULL);
-        printf("Message from server: %s\n", buffer);
+        sendto(client,buffer,sizeof(buffer),0,(struct sockaddr *)&servAddr,sizeof(servAddr));
+        recvfrom(client,buffer,1024,0,NULL,NULL);
+        printf("From server: \n");
+        printf("%s\n",buffer);
     }
-
     close(client);
-    return 0;
+    exit(0);
 }
--- external/udp/server.c
+++ external/udp/server.c
@@ -1,50 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<unistd.h>
 
-int main() {
+int main(){
     int server;
+    struct sockaddr_in servAddr,clientAddr;
+    socklen_t clientAddrSize=sizeof(clientAddr);
     char buffer[1024];
-    struct sockaddr_in servAddr, clientAddr;
-    socklen_t clientAddrSize = sizeof(clientAddr);
+    server=socket(AF_INET,SOCK_DGRAM,0);
+    servAddr.sin_family=AF_INET;
+    servAddr.sin_port=htons(6565);
+    servAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
 
-    server = socket(AF_INET, SOCK_DGRAM, 0);
-
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(6565);
-    servAddr.sin_addr.s_addr = INADDR_ANY;
-
-    if (server == -1) {
+    if(server==-1){
         printf("Socket error..\n");
         exit(1);
-    } else {
+    }
+    else{
         printf("Socket created successfully..\n");
     }
     int x=bind(server,(struct sockaddr *)&servAddr,sizeof(servAddr));
-
-    if (x == -1) {
-        printf("Binding error..\n");
+    if(x==-1){
+        printf("error binding..\n");
         exit(1);
-    } else {
-        printf("Binding successful..\n");
     }
-
-    while (1) {
-        recvfrom(server, buffer, 1024, 0, (struct sockaddr*)&clientAddr, &clientAddrSize);
-        printf("From client: %s\n", buffer);
-        printf("Enter the content to send to client: ");
-        scanf("%s", buffer);
-        if (strcmp(buffer, "exit") == 0) {
-            printf("bye..\n");
+    else{
+        printf("Binded successfully..\n");
+    }
+    while(1){
+        recvfrom(server,buffer,1024,0,(struct sockaddr *)&clientAddr,&clientAddrSize);
+        printf("Message from client: %s\n",buffer);
+        printf("Enter the content to be send to client: \n");
+        scanf("%s",buffer);
+        if(strcmp(buffer,"exit")==0){
             break;
         }
-        sendto(server, buffer, strlen(buffer), 0, (struct sockaddr*)&clientAddr, clientAddrSize);
+        sendto(server,buffer,sizeof(buffer),0,(struct sockaddr *)&clientAddr,clientAddrSize);
     }
-
     close(server);
-    return 0;
+    exit(0);
 }
