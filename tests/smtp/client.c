2d15b1d70665b5901cd7654f401de5fa3a2e5c77
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-08-07 19:39:57 +0530

    done tests

--- tests/smtp/client.c
+++ tests/smtp/client.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<netinet/in.h>
+#include<unistd.h>
 #include<string.h>
 int main(){
     int client;
@@ -12,39 +12,37 @@ int main(){
     char buffer[1024];
     client=socket(AF_INET,SOCK_STREAM,0);
     servAddr.sin_family=AF_INET;
-    servAddr.sin_port=htons(7000);
+    servAddr.sin_port=htons(6000);
     servAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
     if(client<0){
-        printf("Error..\n");
+        printf("Error in socket.\n");
         exit(1);
     }
     else{
-        printf("socket created..\n");
+        printf("Socket created successfully..\n");
     }
-    int z=connect(client,(struct sockaddr *)&servAddr,sizeof(servAddr));
-    if(z<0){
-        printf("Error..\n");
+    int c=connect(client,(struct sockaddr*)&servAddr,sizeof(servAddr));
+    if(c<0){
+        printf("Connection error..\n");
         exit(1);
-    }
-    else{
-        printf("Connnection accepted\n");
     }
     recv(client,buffer,1024,0);
     printf("From server: %s\n",buffer);
     char *emailCommands[]={
         "HELO localhost\r\n",
-        "MAIL FROM: <someone@gmail.com>\r\n",
-        "RCPT TO: <somehow@gmail.com>\r\n",
-        "DATA\r\n",
-        "SUBJECT: Test mail\r\n",
-        "This is a test email\r\n",
+        "MAIL FROM: <someone@example.com>\r\n",
+        "RCPT TO: <somehow@example.com>\r\n",
+        "SUBJECT: Test email\r\n",
+        "This is email body\r\n",
         "\r\n",
         "QUIT\r\n"
-};
-    for(int i=0;i<sizeof(emailCommands)/sizeof(emailCommands[0]);i++){
+    };
+    int n=sizeof(emailCommands)/sizeof(emailCommands[0]);
+    for(int i=0;i<n;i++){
         send(client,emailCommands[i],strlen(emailCommands[i]),0);
         recv(client,buffer,1024,0);
-        printf("From server: %s\n",buffer);
+        puts(buffer);
     }
     close(client);
 }
--- tests/smtp/server.c
+++ tests/smtp/server.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<netinet/in.h>
+#include<unistd.h>
 #include<string.h>
 int main(){
     int server,client;
@@ -12,15 +12,15 @@ int main(){
     char buffer[1024];
     server=socket(AF_INET,SOCK_STREAM,0);
     servAddr.sin_family=AF_INET;
-    servAddr.sin_port=htons(7000);
+    servAddr.sin_port=htons(6000);
     servAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
 
     if(server<0){
-        printf("Error in socket..\n");
+        printf("Error in socket creation..\n");
         exit(1);
     }
     else{
-        printf("Socket created..\n");
+        printf("Socket created successfully..\n");
     }
     int x=bind(server,(struct sockaddr *)&servAddr,sizeof(servAddr));
     if(x<0){
@@ -28,27 +28,24 @@ int main(){
         exit(1);
     }
     else{
-        printf("Binding done\n");
+        printf("Binding successfull..\n");
     }
     if(listen(server,5)==0){
         printf("Listening..\n");
     }
     else{
-        printf("Error\n");
+        printf("Error in listenig..\n");
         exit(1);
     }
-    client=accept(server,(struct sockaddr*)&clientAddr,&clientAddrSize);
-    if(client<0){
-        printf("Error in accept..\n");
-    }
-    strcpy(buffer,"220 Welcome to SMTP server\r\n");
-    send(client,buffer,sizeof(buffer),0);
+    client=accept(server,(struct sockaddr *)&clientAddr,&clientAddrSize);
+    char *welcomeMessage="Welcome to SMTP server\r\n";
+    send(client,welcomeMessage,strlen(welcomeMessage),0);
     while(1){
         recv(client,buffer,1024,0);
-        printf("From client: %s\n",buffer);
+        printf("Client: %s\n",buffer);
         if(strstr(buffer,"QUIT")!=NULL){
-            strcpy(buffer,"221 Good bye\r\n");
-            send(client,buffer,sizeof(buffer),0);
+            char *response="Good Bye\r\n";
+            send(client,response,strlen(response),0);
             break;
         }
         else{
@@ -56,7 +53,7 @@ int main(){
             send(client,response,strlen(response),0);
         }
     }
-    close(server);
     close(client);
+    close(server);
     exit(0);
 }
