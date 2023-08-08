9b0a1eccdc03a4b5dd65fdaa75783eb815c00537
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-08-08 14:07:31 +0530

    leaky done

--- tests/leaky bucket/leaky.c
+++ tests/leaky bucket/leaky.c
@@ -1,28 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
-    int bucket_size,occupied=0,rate,n;
-    printf("Enter the number of inputs: \n");
-    scanf("%d",&n);
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int occuppied = 0, bucket_size, n,output_rate;
     printf("Enter the bucket size: \n");
-    scanf("%d",&bucket_size);
-    printf("Enter the rate: \n");
-    scanf("%d",&rate);
-    while(n!=0){
-        printf("Enter the input size: \n");
+    scanf("%d", &bucket_size);
+    printf("Enter the number of inputs: \n");
+    scanf("%d", &n);
+    printf("Enter the output rate: \n");
+    scanf("%d",&output_rate);
+    while (n != 0)
+    {
+        printf("Enter the size: \n");
         int in;
-        scanf("%d",&in);
-        if(in<=(bucket_size-occupied)){
-            occupied+=in;
-            printf("Status:\noccupied: %d\t total size: %d\n",occupied,bucket_size);
+        scanf("%d", &in);
+        if (in <= (bucket_size - occuppied))
+        {
+            occuppied += in;
+            printf("Status: \noccupied: %d\t total: %d\n", occuppied, bucket_size);
         }
-        else{
-            printf("Removing %d\n",in-(bucket_size-occupied));
-            occupied=bucket_size;
-            printf("Status:\noccupied: %d\t total size: %d\n",occupied,bucket_size);
+        else
+        {
+            printf("discarding: %d\n", (in - (bucket_size - occuppied)));
+            occuppied = bucket_size;
+            printf("Status: \noccupied: %d\t total: %d\n", occuppied, bucket_size);
         }
-        occupied-=in;
-        printf("Status after removal:\noccupied: %d\t total size: %d\n",occupied,bucket_size);
+        occuppied-=output_rate;
+        printf("status after discarding..\n occupied: %d\t total: %d\n",occuppied,bucket_size);
         n--;
     }
 }
