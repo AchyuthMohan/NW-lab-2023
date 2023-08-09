a94caa0c3d6708ce565088e291695abdd7065072
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-08-09 05:51:33 +0530

    done

--- tests/go back n/client.c
+++ tests/go back n/client.c
@@ -8,59 +8,57 @@
 int main()
 {
     int client;
-    struct sockaddr_in servAddr, clientAddr;
+    struct sockaddr_in servAddr, clientAddr ;
     socklen_t clientAddrSize = sizeof(clientAddr);
-    int frame_count = 8;
+
     client = socket(AF_INET, SOCK_STREAM, 0);
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(1234);
+    servAddr.sin_family = AF_INET,
+    servAddr.sin_port = htons(6655);
     servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if (client < 0)
+    if (client <= 0)
     {
-        printf("Error in socket..\n");
+        printf("socket error\n");
         exit(1);
     }
     else
     {
-        printf("Socket created successfully..\n");
+        printf("Socket created.\n");
     }
     int c = connect(client, (struct sockaddr *)&servAddr, sizeof(servAddr));
     if (c < 0)
     {
-        printf("Connect request error..\n");
+        printf("Connection error\n");
         exit(1);
     }
     else
     {
-        printf("Connection success..\n");
+        printf("Connected..\n");
     }
-    int windw_size = 4;
+    int window_size = 4;
+    int frame_count = 10;
     int send_frame = 0;
-    int nextframe_to_send = 0;
     int ack_frame = 0;
+    int next_frame_to_send = 0;
     while (send_frame < frame_count)
     {
-        while (nextframe_to_send < frame_count && nextframe_to_send < send_frame + windw_size)
+        while (next_frame_to_send < frame_count && next_frame_to_send < window_size + send_frame)
         {
-            printf("Sending frame: %d\n", nextframe_to_send);
-            send(client, &nextframe_to_send, sizeof(nextframe_to_send), 0);
-            nextframe_to_send++;
+            printf("sending the next frame %d\n", next_frame_to_send);
+            send(client, &next_frame_to_send, sizeof(next_frame_to_send), 0);
+            next_frame_to_send ++ ;
         }
-
         while (ack_frame <= send_frame)
         {
-            int recved_ack;
-            recv(client, &recved_ack, sizeof(recved_ack), 0);
-            printf("Acknowledgement: %d\n", recved_ack);
-            ack_frame = (recved_ack + 1) % frame_count;
+            int recvd_frame;
+            recv(client, &recvd_frame, sizeof(recvd_frame), 0);
+            printf("Acknowledgement: %d\n", recvd_frame);
+            ack_frame = (recvd_frame + 1) % frame_count;
         }
-        send_frame = ack_frame;
-        if (send_frame == frame_count - 1)
-        {
+        send_frame=ack_frame;
+        if(send_frame==frame_count-1){
             break;
         }
     }
     close(client);
-    exit(0);
 }
--- tests/go back n/server.c
+++ tests/go back n/server.c
@@ -1,80 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <string.h>
-int main()
-{
-    int server, client;
-    struct sockaddr_in servAddr, clientAddr;
-    socklen_t clientAddrSize = sizeof(clientAddr);
-    int frame_count = 8;
-    server = socket(AF_INET, SOCK_STREAM, 0);
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(1234);
-    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<string.h>
+int main(){
+    int server,client;
+    struct sockaddr_in servAddr,clientAddr;
+    socklen_t clientAddrSize=sizeof(clientAddr);
 
-    if (server < 0)
-    {
-        printf("Error in socket..\n");
+    server=socket(AF_INET,SOCK_STREAM,0);
+    servAddr.sin_family=AF_INET,
+    servAddr.sin_port=htons(6655);
+    servAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+    if(server<=0){
+        printf("socket error\n");
         exit(1);
     }
-    else
-    {
-        printf("Socket created successfully..\n");
+    else{
+        printf("Socket created.\n");
     }
-    int b = bind(server, (struct sockaddr *)&servAddr, sizeof(servAddr));
-    if (b < 0)
-    {
-        printf("Error in binding..\n");
+    int b=bind(server,(struct sockaddr *)&servAddr,sizeof(servAddr));
+    if(b<0){
+        printf("binding error\n");
         exit(1);
     }
-    else
-    {
-        printf("Binding done..\n");
+    else{
+        printf("Binded successfully..\n");
     }
-    if (listen(server, 5) == 0)
-    {
+    if(listen(server,5)==0){
         printf("Listening..\n");
     }
-    else
-    {
-        printf("Error in listening..\n");
+    else{
+        printf("Listening error..\n");
         exit(1);
     }
-    client = accept(server, (struct sockaddr *)&clientAddr, &clientAddrSize);
-    if (client < 0)
-    {
-        printf("Error in accept..\n");
+    client=accept(server,(struct sockaddr *)&clientAddr,&clientAddrSize);
+    if(client<0){
+        printf("accept error \n");
         exit(1);
     }
-    else
-    {
-        printf("Connected succesfully..\n");
+    else{
+        printf("accepted..\n");
     }
-    int expected_frame = 0;
-    while (expected_frame < frame_count)
-    {
-        int received_frame;
-        recv(client, &received_frame, sizeof(received_frame), 0);
-        if (received_frame == expected_frame)
-        {
-            printf("Frame received: %d\n", received_frame);
-            printf("Sending acknowledgement for %d\n", expected_frame);
-            send(client, &expected_frame, sizeof(expected_frame), 0);
-            expected_frame = expected_frame+1;
+    int expected_frame=0;
+    int frame_count=10;
+    while(expected_frame<frame_count){
+        int recvd_frame;
+        recv(client,&recvd_frame,sizeof(recvd_frame),0);
+        if(recvd_frame==expected_frame){
+            printf("received frame: %d\n",recvd_frame);
+            printf("sending acknowledgement %d\n",expected_frame);
+            send(client,&expected_frame,sizeof(expected_frame),0);
+            expected_frame++;
         }
-        else
-        {
-            printf("Discarding..%d\n", received_frame);
+        else{
+            printf("Discarding..frame : %d\n",expected_frame);
         }
-        if (received_frame == frame_count - 1)
-        {
+        if(expected_frame==frame_count-1){
             break;
         }
     }
-    close(client);
     close(server);
+    close(client);
 }
