bf006ddefd7626a40edb4551fb06d85a87052d8f
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-08-09 06:06:15 +0530

    multi client

--- tests/multi-client-tcp/client1.c
+++ tests/multi-client-tcp/client1.c
@@ -1,38 +1,40 @@
-// client.c
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <unistd.h>
-
-int main() {
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<unistd.h>
+int main(){
     int client;
-    struct sockaddr_in servAddr;
+    struct sockaddr_in servAddr,clientAddr;
+    socklen_t clientAddrSize=sizeof(clientAddr);
     char buffer[1024];
-
-    client = socket(AF_INET, SOCK_STREAM, 0);
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    servAddr.sin_port = htons(6655);
-
-    if (client < 0) {
+    client=socket(AF_INET,SOCK_STREAM,0);
+    servAddr.sin_family=AF_INET;
+    servAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    servAddr.sin_port=htons(8080);
+    if(client<0){
         printf("Error\n");
         exit(1);
     }
-
-    if (connect(client, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
-        printf("Error\n");
+    else{
+        printf("Socket created..\n");
+    }
+    int c=connect(client,(struct sockaddr *)&servAddr,sizeof(servAddr));
+    if(c<0){
+        printf("error\n");
         exit(1);
     }
-
-    while (1) {
-        printf("Enter the message: ");
-        fgets(buffer, sizeof(buffer), stdin);
-        send(client, buffer, sizeof(buffer), 0);
+    else{
+        printf("Connected..\n");
     }
-
+    while(1){
+        printf("Enter the message: \n");
+        scanf("%s",buffer);
+        send(client,buffer,sizeof(buffer),0);
+        recv(client,buffer,1024,0);
+        printf("From server: %s\n",buffer);
+    }
     close(client);
-    return 0;
 }
--- tests/multi-client-tcp/server.c
+++ tests/multi-client-tcp/server.c
@@ -1,96 +1,86 @@
-// server.c (Multi-Client Chat Server)
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
 
+#define PORT 8080
 #define MAX_CLIENTS 10
+#define MAX_BUFFER 1024
 
 int main() {
-    int server, client;
-    struct sockaddr_in servAddr, clientAddr;
-    socklen_t clientAddrSize = sizeof(clientAddr);
-    int clients[MAX_CLIENTS];
-    fd_set read_fds;
-    int max_fd;
+    int sockfd, newsockfd, clients[MAX_CLIENTS], client_count = 0;
+    struct sockaddr_in servaddr, cliaddr;
+    socklen_t len;
 
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        clients[i] = -1; // Initialize client socket descriptors
+    // Create a socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    server = socket(AF_INET, SOCK_STREAM, 0);
-    if (server < 0) {
-        printf("Error in socket creation..\n");
-        exit(1);
+    // Set server address
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) {
+        perror("socket bind failed");
+        exit(EXIT_FAILURE);
     }
 
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.s_addr = INADDR_ANY;
-    servAddr.sin_port = htons(6655);
-
-    if (bind(server, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
-        printf("Error in binding..\n");
-        exit(1);
+    // Listen for incoming connections
+    if (listen(sockfd, 5) != 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
     }
 
-    if (listen(server, MAX_CLIENTS) == 0) {
-        printf("Listening..\n");
-    } else {
-        printf("Listening error..\n");
-        exit(1);
-    }
+    fd_set active_fds, read_fds;
+    FD_ZERO(&active_fds);
+    FD_SET(sockfd, &active_fds);
+
+    printf("Server is listening...\n");
 
     while (1) {
-        FD_ZERO(&read_fds);
-        FD_SET(server, &read_fds);
-        max_fd = server;
-
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-            if (clients[i] != -1) {
-                FD_SET(clients[i], &read_fds);
-                if (clients[i] > max_fd) {
-                    max_fd = clients[i];
-                }
-            }
+        read_fds = active_fds;
+        if (select(FD_SETSIZE, &read_fds, NULL, NULL, NULL) == -1) {
+            perror("select");
+            exit(EXIT_FAILURE);
         }
 
-        select(max_fd + 1, &read_fds, NULL, NULL, NULL);
+        for (int i = 0; i < FD_SETSIZE; ++i) {
+            if (FD_ISSET(i, &read_fds)) {
+                if (i == sockfd) {
+                    len = sizeof(cliaddr);
+                    newsockfd = accept(sockfd, (struct sockaddr*)&cliaddr, &len);
+                    if (newsockfd < 0) {
+                        perror("server accept failed");
+                        exit(EXIT_FAILURE);
+                    }
 
-        if (FD_ISSET(server, &read_fds)) {
-            client = accept(server, (struct sockaddr *)&clientAddr, &clientAddrSize);
-            if (client < 0) {
-                printf("Accept error\n");
-                exit(1);
-            }
+                    FD_SET(newsockfd, &active_fds);
+                    clients[client_count++] = newsockfd;
 
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (clients[i] == -1) {
-                    clients[i] = client;
-                    printf("Client %d connected.\n", client);
-                    break;
-                }
-            }
-        }
-
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-            if (clients[i] != -1 && FD_ISSET(clients[i], &read_fds)) {
-                char buffer[1024];
-                memset(buffer, 0, sizeof(buffer));
-
-                int bytes_received = recv(clients[i], buffer, sizeof(buffer), 0);
-                if (bytes_received <= 0) {
-                    printf("Client %d disconnected.\n", clients[i]);
-                    close(clients[i]);
-                    clients[i] = -1;
+                    printf("Client %d connected\n", newsockfd);
                 } else {
-                    printf("From client %d: %s\n", clients[i], buffer);
-                    // Broadcast the message to other clients
-                    for (int j = 0; j < MAX_CLIENTS; j++) {
-                        if (clients[j] != -1 && clients[j] != clients[i]) {
-                            send(clients[j], buffer, bytes_received, 0);
+                    char buffer[MAX_BUFFER];
+                    int n = read(i, buffer, sizeof(buffer));
+                    if (n <= 0) {
+                        printf("Client %d disconnected\n", i);
+                        close(i);
+                        FD_CLR(i, &active_fds);
+                    } else {
+                        buffer[n] = '\0';
+                        printf("Received from client %d: %s", i, buffer);
+
+                        // Broadcast the message to other clients
+                        for (int j = 0; j < client_count; ++j) {
+                            if (clients[j] != i) {
+                                write(clients[j], buffer, n); // Send the whole buffer
+                            }
                         }
                     }
                 }
@@ -98,6 +88,7 @@ int main() {
         }
     }
 
-    close(server);
+    close(sockfd);
+
     return 0;
 }
