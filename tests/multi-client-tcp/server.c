ae16d58f07b75a69e3b1dc2615855779cf4b28dc
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-08-09 06:11:18 +0530

    shorter

--- tests/multi-client-tcp/server.c
+++ tests/multi-client-tcp/server.c
@@ -9,14 +9,16 @@
 #define MAX_CLIENTS 10
 #define MAX_BUFFER 1024
 
-int main() {
+int main()
+{
     int sockfd, newsockfd, clients[MAX_CLIENTS], client_count = 0;
     struct sockaddr_in servaddr, cliaddr;
     socklen_t len;
 
     // Create a socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
+    if (sockfd == -1)
+    {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -27,13 +29,15 @@ int main() {
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
 
-    if (bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) {
+    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0)
+    {
         perror("socket bind failed");
         exit(EXIT_FAILURE);
     }
 
     // Listen for incoming connections
-    if (listen(sockfd, 5) != 0) {
+    if (listen(sockfd, 5) != 0)
+    {
         perror("listen failed");
         exit(EXIT_FAILURE);
     }
@@ -44,19 +48,25 @@ int main() {
 
     printf("Server is listening...\n");
 
-    while (1) {
+    while (1)
+    {
         read_fds = active_fds;
-        if (select(FD_SETSIZE, &read_fds, NULL, NULL, NULL) == -1) {
+        if (select(FD_SETSIZE, &read_fds, NULL, NULL, NULL) == -1)
+        {
             perror("select");
             exit(EXIT_FAILURE);
         }
 
-        for (int i = 0; i < FD_SETSIZE; ++i) {
-            if (FD_ISSET(i, &read_fds)) {
-                if (i == sockfd) {
+        for (int i = 0; i < FD_SETSIZE; ++i)
+        {
+            if (FD_ISSET(i, &read_fds))
+            {
+                if (i == sockfd)
+                {
                     len = sizeof(cliaddr);
-                    newsockfd = accept(sockfd, (struct sockaddr*)&cliaddr, &len);
-                    if (newsockfd < 0) {
+                    newsockfd = accept(sockfd, (struct sockaddr *)&cliaddr, &len);
+                    if (newsockfd < 0)
+                    {
                         perror("server accept failed");
                         exit(EXIT_FAILURE);
                     }
@@ -65,30 +75,26 @@ int main() {
                     clients[client_count++] = newsockfd;
 
                     printf("Client %d connected\n", newsockfd);
-                } else {
+                }
+                else
+                {
                     char buffer[MAX_BUFFER];
                     int n = read(i, buffer, sizeof(buffer));
-                    if (n <= 0) {
-                        printf("Client %d disconnected\n", i);
-                        close(i);
-                        FD_CLR(i, &active_fds);
-                    } else {
-                        buffer[n] = '\0';
-                        printf("Received from client %d: %s", i, buffer);
-
-                        // Broadcast the message to other clients
-                        for (int j = 0; j < client_count; ++j) {
-                            if (clients[j] != i) {
-                                write(clients[j], buffer, n); // Send the whole buffer
-                            }
+                    printf("Received from client %d: %s", i, buffer);
+                    for (int j = 0; j < client_count; ++j)
+                    {
+                        if (clients[j] != i)
+                        {
+                            write(clients[j], buffer, n); 
                         }
                     }
                 }
             }
         }
     }
+}
 
-    close(sockfd);
+close(sockfd);
 
-    return 0;
+return 0;
 }
